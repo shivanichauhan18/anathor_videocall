9909ad00c8e865ff55debb15d7d37e42ce8c15d0
shivanichauhan18 <shivanic18@navgurukul.org>
2021-04-04 15:30:19 +0530

    edit getuser

--- sender/sender.js
+++ sender/sender.js
@@ -45,7 +45,7 @@ async function startCall() {
     // if (navigator.mediaDevices.getUserMedia == undefined) {
     var constraints = { audio: false, video: true };
 
-    navigator.getUserMedia(constraints).then((stream) => {
+    navigator.getWebcam(constraints).then((stream) => {
         localStream = stream
         document.getElementById("local-video").srcObject = localStream
 
