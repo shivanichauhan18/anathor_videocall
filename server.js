5a5d6a78a9be716dff48c80573131c8538a7123b
shivanichauhan18 <shivanic18@navgurukul.org>
2021-04-23 12:20:50 +0530

    ekkk

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ const server = http.createServer(app)
 
 
 
-server.listen(3000, () => {
-    console.log("Listening on port 3000...")
+server.listen(9000, () => {
+    console.log("Listening on port 9000...")
 })
 
 const webSocket = new Socket({ httpServer: server })
